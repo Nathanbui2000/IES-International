a61bc523eb887948091cf478c19909946f46406f
Hoang Bui <hbui6373@uni.sydney.edu.au>
2022-10-12 18:29:07 +1100

    NavBar

---
+++ ies-project/src/components/Header/Header.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import ".//Header.css";
+const nav_links = [
+    {
+        path:"#home",
+        display:"Home"
+    },
+    {
+        path:"#about",
+        display:"About"
+    },
+    {
+        path:"#home",
+        display:"Home"
+    },
+    {
+        path:"#service",
+        display:"Service"
+    },
+    {
+        path:"#project",
+        display:"Project"
+    },
+    {
+        path:"#blog",
+        display:"Blog"
+    },   
+]
+const Header = () =>
+{
+    return (
+        <header className = 'header'>
+            <div className= 'container'>
+                <div className = 'nav_wrapper'>
+                    <div className = 'logo'>
+                        <h2>Digency</h2>
+                    </div>
+                    {/* =========== navigation===========*/}
+                    <div className = 'navigation'>
+                        <ul className = 'menu'> 
+                        {
+                            nav_links.map((item,index) => (
+                                <li className ='menu_item'>
+                                    <a 
+                                    href ={item.path}
+                                    className = 'menu_link'>
+                                        {item.display}
+                                    </a>
+                                </li>
+                            ))
+                        }
+                        </ul>
+                    </div>
+
+                    {/*=============== Light Mode =========================*/}
+                    <div className ="light_mode">
+                        <span>
+                            <i class="ri-sun-line">Light Mode</i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+export default Header
---
+++ ies-project/src/components/UI/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "../../styples/hero.css"
+
+const Hero = () => 
+{
+    return <secction className = 'hero_sections'>
+        <div>
+            <div className = 'hero_wrapper'>
+                <div className = 'hero_img'>
+                    <h2>
+                        
+                    </h2>
+
+                </div>
+
+            </div>
+        </div>
+    </secction>
+}
+export default Hero;
